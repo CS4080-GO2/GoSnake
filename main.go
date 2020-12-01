409374ec911bc2701a5c7d4479452e52cb6c5c50
jdao25 <johnnydao4@gmail.com>
2020-11-30 19:37:21 -0800

    Initialized termbox library

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import(
     "fmt"
-//    "github.com/nsf/termbox-go"
+    "github.com/nsf/termbox-go"
 )
 
 
 func main() {
+    // Initualizes termbox library
+    err := termbox.Init()
+
+    // If an error occur, then panic
+    if err != nil {
+        // If fail, stop all function and go to defer functions
+        panic(err)
+    }
+
+    // Ensure that termbox is always close
+    defer termbox.Close()
+
     fmt.Println()
 }
