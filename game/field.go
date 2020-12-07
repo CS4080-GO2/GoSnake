a262519b8f51d49ecac4382bb547df16ac52dafe
jdao25 <johnnydao4@gmail.com>
2020-12-07 02:42:39 -0800

    Updating master

--- game/field.go
+++ game/field.go
@@ -1,14 +1,180 @@
 package game
 
 import (
-    "github.com/nsf/termbox-go"
-
+	"math/rand"
+	"time"
+	"fmt"
+	"github.com/nsf/termbox-go"
+	rune "github.com/mattn/go-runewidth"
 )
 
-
 type Field struct {
-    food    Food
-    snake   Snake
-    height  int
-    width   int
+	food   	Food
+	snake  	Snake
+	height 	int
+	width	int
+	points	int
+}
+
+const (
+	fieldWidth  = 60
+	fieldHeight = 24
+	WallColor   = termbox.ColorCyan
+	EmptyColor  = termbox.ColorDefault
+	SnakeColor  = termbox.ColorRed
+)
+
+var width int
+var height int
+
+func InitField() Field {
+	rand.Seed(time.Now().UnixNano())
+
+	f := Field {
+		snake: 	InitSnake(fieldWidth, fieldHeight),
+		height: fieldHeight,
+		width: 	fieldWidth,
+	}
+
+	f.PlaceFood()
+
+	return f
+}
+
+// Display draws the field, snake, and food.
+func (f *Field) Display() {
+	// Clear screen.
+	termbox.Clear(EmptyColor, EmptyColor)
+
+	// Make border
+	DrawBorder()
+	DrawFood(f.food)
+	DrawScore(f.points)
+	DrawMsg(fieldWidth + 5, fieldHeight - 1, "Press ESC to exit")
+	drawSnake(&f.snake)
+
+	// Now display it
+	termbox.Flush()
+}
+
+func drawSnake(s *Snake) {
+	for i := 0; i < len(s.body); i++ {
+		termbox.SetCell(s.body[i].x, s.body[i].y, ' ', SnakeColor, SnakeColor)
+	}
+}
+
+func DrawBorder() {
+	width, height = termbox.Size()
+
+	// Make bottom
+	for x := 0; x < fieldWidth + 1; x++ {
+		termbox.SetCell(x, fieldHeight, ' ', WallColor, WallColor)
+	}
+
+	// Make top
+	for x := 0; x < fieldWidth + 1; x++ {
+		termbox.SetCell(x, 0, ' ', WallColor, WallColor)
+	}
+
+	// Make right
+	for y := 1; y < fieldHeight; y++ {
+		termbox.SetCell(fieldWidth, y, ' ', WallColor, WallColor)
+	}
+
+	// Make left
+	for y := 1; y < fieldHeight; y++ {
+		termbox.SetCell(0, y, ' ', WallColor, WallColor)
+	}
+}
+
+// Function for snake movement
+func (f *Field) move() {
+	head := f.snake.body[0]
+	c := Coordinate{x: head.x, y: head.y}	// New position of the head
+
+	switch f.snake.direction {
+	case UP:
+		f.snake.moveBody(Coordinate{x: head.x, y: head.y - 1})
+		c.y -= 1
+	case DOWN:
+		f.snake.moveBody(Coordinate{x: head.x, y: head.y + 1})
+		c.y += 1
+	case LEFT:
+		f.snake.moveBody(Coordinate{x: head.x - 1, y: head.y})
+		c.x -= 1
+	case RIGHT:
+		f.snake.moveBody(Coordinate{x: head.x + 1, y: head.y})
+		c.x += 1
+	}
+
+	// Check if the snake hit its body
+
+	if f.snake.CheckHeadPosition(c) {	// Check if head position is on body
+		// End the game, since head hit body
+		GameOver("You hit your body!", f.points)
+	}
+
+	// If the snake ate the food
+	if c == f.food.coord {
+		go f.AddPoint(100)
+		f.snake.length += 1
+		f.snake.body = append(f.snake.body, c)
+		f.PlaceFood()
+	}
+
+	// If the snake exit the field then display "Game Over"
+	f.SnakeExit()
+}
+
+// Need to use goroutine to add the point
+func (f *Field) AddPoint(point int) {
+	f.points += 100
+}
+
+// Function for when the snake leaves the field
+func (f *Field) SnakeExit() {
+	head := f.snake.body[0]
+
+	if head.x >= fieldWidth || head.y >= fieldHeight ||
+		head.x <= 0 || head.y <= 0 {
+		// If the leaves the field, it's game over
+		GameOver("You're leaving the field?!", f.points)
+	}
+}
+
+func DrawMsg(x, y int, msg string) {
+	clr := termbox.ColorDefault
+
+	for _, c := range msg {
+		termbox.SetCell(x, y, c, clr, clr)
+		x += rune.RuneWidth(c)
+	}
+}
+
+func (f *Field) PlaceFood() {
+	// Declare x and y coord for the rand food drop
+	var randCoord Coordinate
+
+	for {
+		x := rand.Intn(fieldWidth - 2) + 1
+		y := rand.Intn(fieldHeight - 2) + 1
+
+		randCoord = Coordinate{x: x, y: y}
+		if f.snake.AvailablePosition(randCoord) {
+			break
+		}
+	}
+
+	f.food = DropFoodAt(randCoord)
+}
+
+
+func DrawFood(f Food) {
+	clr := termbox.ColorDefault
+	termbox.SetCell(f.coord.x, f.coord.y, f.char, clr, clr)
+}
+
+func DrawScore(score int) {
+	msg := fmt.Sprintf("Score: %v", score)
+	DrawMsg(fieldWidth + 5, 1, msg)	// Display the score
 }
--- game/snake.go
+++ game/snake.go
@@ -1,12 +1,73 @@
 package game
 
-import (
-    "github.com/nsf/termbox-go"
 
+type direction int
+
+const InitialSnakeLength = 4
+
+// Snake the actual snake
+type Snake struct {
+	body      []Coordinate // Snake body
+	length    int          // Snake Length
+	direction direction    // Direction snake is facing
+}
+
+// Got help from https://programming.guide/go/define-enumeration-string.html and https://golangbyexample.com/iota-in-golang/
+const (
+	UP direction = iota
+	DOWN
+	LEFT
+	RIGHT
 )
 
+func InitSnake(w, h int) Snake {
+	var temp []Coordinate
 
-type Snake struct {
-    body    []int   // Snake body
-    length  int      // Snake Length
+	for y := 0; y < InitialSnakeLength; y++ {
+		temp = append(temp, Coordinate{x: w / 2, y: (h / 2) + y}) // Essentially, we want body[0] to be the head, pointing upwards.
+	}
+
+	return Snake{
+		body:      temp,
+		length:    InitialSnakeLength,
+		direction: UP,
+	}
+}
+
+func (s *Snake) moveBody(coord Coordinate) {
+	// A little jank...
+	// Have to make slice without 'make' because it always allocates too much memory over and over due to the len(slice) not actually representing the number of elements in the slice.
+	var temp []Coordinate
+	temp = append(temp, coord)
+	for i := 0; i < len(s.body)-1; i++ { // Copy all except last element over, tail needs to be forgotten.
+		temp = append(temp, s.body[i])
+	}
+
+	s.body = temp
+}
+
+// Function to check if the Coordinate is on the snake body
+func (s *Snake) CheckHeadPosition(c Coordinate) bool {
+	// Start at s.body[1:] since we want to check the body not including the
+	// head
+	for _, body := range s.body[1:] {
+		// Check if the coord of the head match one coord in body
+		if c.x == body.x && c.y == body.y {
+			return true
+		}
+	}
+	return false
+}
+
+// Check body position for random food drops
+func (s *Snake) AvailablePosition(c Coordinate) bool {
+	// Traverse through snake body to see if the randomly selected Coordinate
+	// is available
+	for _, body := range s.body {
+		// Check if the coord of the head match one coord in body
+		if c.x != body.x && c.y != body.y {
+			return true
+		}
+	}
+	return false
 }
